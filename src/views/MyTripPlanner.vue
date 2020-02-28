5f17e5ad0d7cf58ab1d8818ed976a881f1b166ba
Duncan Sanderson <duncan@duncansanderson.co.uk>
2020-02-28 01:05:17 +0000

    FEAT (MyTripPlanner) Add page

--- src/views/MyTripPlanner.vue
+++ src/views/MyTripPlanner.vue
@@ -1,3 +1,60 @@
 <template>
-    <h1>MyTripPlanner</h1>
+    <div>
+        <b-list-group v-if="likedArticles.length > 0">
+            <template v-for="(article, index) in articles">
+                <b-list-group-item
+                    :key="index"
+                    v-if="likedArticles.indexOf(article._id) >= 0"
+                >
+                    <router-link :to="`/article/${article._id}`">{{ article.name }}</router-link>
+                    <b-icon-trash
+                        class="icon"
+                        @click="unlikeArticle(article._id)"
+                        :font-scale="2"
+                    ></b-icon-trash>
+                </b-list-group-item>
+            </template>
+        </b-list-group>
+        <b-card-text v-if="likedArticles.length == 0">No articles liked.</b-card-text>
+    </div>
 </template>
+
+<script>
+import { BListGroup, BListGroupItem, BCardText, BIconTrash } from 'bootstrap-vue'
+import { mapGetters } from 'vuex'
+
+export default {
+    components: {
+        BListGroup,
+        BListGroupItem,
+        BCardText,
+        BIconTrash
+    },
+    computed: {
+        ...mapGetters([
+            'isLoaded',
+            'articles',
+            'likedArticles'
+        ]),
+    },
+    methods: {
+        unlikeArticle(articleId) {
+            this.$store.dispatch('unlikeArticle', articleId)
+        },
+    },
+
+}
+</script>
+
+<style lang="scss" scoped>
+.list-group-item {
+    position: relative;
+
+    .icon {
+        cursor: pointer;
+        position: absolute;
+        top: 8px;
+        right: 20px;
+    }
+}
+</style>
